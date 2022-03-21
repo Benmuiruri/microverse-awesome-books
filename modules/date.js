df47c382f28a6a81ad6586cd68109128d608031c
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-21 12:33:48 +0300

    Fix js Lint error

--- modules/date.js
+++ modules/date.js
@@ -2,7 +2,7 @@ import { DateTime } from './luxon.js';
 
 const DisplayTime = () => {
   document.getElementById('date').innerHTML = DateTime.now().toLocaleString(
-    DateTime.DATETIME_MED_WITH_WEEKDAY
+    DateTime.DATETIME_MED_WITH_WEEKDAY,
   );
   setInterval(DisplayTime, 1000);
 };
